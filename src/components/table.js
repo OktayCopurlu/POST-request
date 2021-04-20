48506432e74377b08230872479868c3e418c47ea
OktayCopurlu <selimhalim12@gmail.com>
2021-04-20 22:32:44 +0200

    first commit

---
+++ src/components/table.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Table } from "react-bootstrap";
+import { useState, useEffect } from "react";
+
+export default function Tables() {
+
+  const [status, setStatus] = useState([]);
+
+  useEffect(() => {
+    const getReport = async () => {
+      const response = await fetch("http://174.138.103.233/api/employees");
+      const dataJson = await response.json();
+
+      setStatus(dataJson);
+    };
+    getReport();
+  }, []);
+  return (
+    <>
+      <div className="container">
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Gender</th>
+              <th>Birthday</th>
+              <th>About</th>
+            </tr>
+          </thead>
+          <tbody>
+            {status.map((element,index) => {
+              return (
+                <tr>
+                  <td>{index}</td>
+                  <td>{element.firstName}</td>
+                  <td>{element.lastName}</td>
+                  <td>{element.email}</td>
+                  <td>{element.gender}</td>
+                  <td>{element.birthday}</td>
+                  <td>{element.about}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </div>
+    </>
+  );
+}
